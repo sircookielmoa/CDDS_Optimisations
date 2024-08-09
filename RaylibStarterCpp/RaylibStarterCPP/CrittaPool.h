76d1c3ee850e90a378c86e894cac3d2935f5e575
ed Sheeran <99225359+sircookielmoa@users.noreply.github.com>
2024-08-09 16:37:16 +1000

    crittapoolh
    headafile

--- RaylibStarterCpp/RaylibStarterCPP/CrittaPool.h
+++ RaylibStarterCpp/RaylibStarterCPP/CrittaPool.h
@@ -1,5 +1,14 @@
 #pragma once
 class CrittaPool
 {
+public:
+	void init(double x, double y, double xVel, double yVel, int lifetime);
+	void animate();
+	bool isAlive() const;
+
+private:
+	int framesleft_;
+	double x_, y_;
+	double xVel_, yVel_;
 };
 
